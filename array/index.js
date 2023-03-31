dbdc6888b8ebb68716a5d2123eb4a4b39f5d67f7
wdf <wdf332@gmail.com>
2023-03-31 23:01:09 +0800

    array

---
+++ array/index.js
@@ -0,0 +1,28 @@
+"use strict";
+let numbers = [1, 2, 3, 4, 5];
+let strings = ['hi', 'how are you', 'goodbye'];
+let asd = {
+    brand: 232,
+    type: 'asd',
+    color: 'asd',
+    manufactureDate: Date
+};
+var dsasd;
+(function (dsasd) {
+    dsasd[dsasd["a"] = 0] = "a";
+    dsasd[dsasd["b"] = 1] = "b";
+    dsasd[dsasd["c"] = 2] = "c";
+    dsasd[dsasd["d"] = 3] = "d";
+    dsasd[dsasd["e"] = 4] = "e";
+    dsasd[dsasd["f"] = 5] = "f";
+    dsasd[dsasd["g"] = 6] = "g";
+    dsasd[dsasd["h"] = 7] = "h";
+    dsasd[dsasd["i"] = 8] = "i";
+    dsasd[dsasd["j"] = 9] = "j";
+    dsasd[dsasd["k"] = 10] = "k";
+    dsasd[dsasd["l"] = 11] = "l";
+})(dsasd || (dsasd = {}));
+let asd2 = dsasd.c;
+console.log(asd2);
+let asd3 = dsasd[asd2];
+console.log(asd3);
---
+++ array/index.ts
@@ -0,0 +1,27 @@
+
+
+let numbers = [1, 2, 3, 4, 5];
+
+let strings = ['hi', 'how are you', 'goodbye'];
+
+let asd = {
+  brand: 232,
+  type: 'asd',
+  color: 'asd',
+  manufactureDate: Date
+}
+type aass = {
+  aa: string,
+  bb: boolean,
+  cc: number,
+  dd: dsasd | undefined
+
+}
+// enum
+enum dsasd { a, b, c, d, e, f, g, h, i, j, k, l }
+
+let asd2: dsasd = dsasd.c
+console.log(asd2)
+
+let asd3 = dsasd[asd2]
+console.log(asd3)
